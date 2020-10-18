f229e8c9a32abae803c7a4e7b9fbe299c00400c4
ProcessX <luc2526@gmail.com>
2020-10-18 11:18:42 +0200

    First commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+
+<style lang="scss" global>
+	p{
+		color: red;
+	}
+</style>
+
+<h1>About this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
