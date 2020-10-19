fc0d90650241b65e530a06e447d17aa607b3492a
ProcessX <luc2526@gmail.com>
2020-10-19 10:40:59 +0200

    First API request

--- src/components/FilmHightlight.svelte
+++ src/components/FilmHightlight.svelte
@@ -1,13 +1,52 @@
 <script>
-    // your script goes here
+    import { onMount } from 'svelte';
+
+    const apiKey = `d8aa152441e9d9c755576dd9bbf66733`;
+    const apiUrl = `https://api.themoviedb.org/3/movie/550`;
+
+    let movieData;
+
+
+    //-------------------------------------------Init---------------------------------
+/*
+    onMount(async () => {
+        getFilmHighlight();
+    })
+*/
+    //-------------------------------------------FUNCTIONS---------------------------------
+
+    function getFilmHighlight(){
+
+        let requestUrl = `${apiUrl}?api_key=${apiKey}`;
+        console.log(requestUrl);
+        console.log("Get film hightlight");
+
+        fetch(requestUrl)
+        .then(
+            (response) => {
+                return response.json();
+            },
+            (response) => {
+                console.log("Request failed");
+            }
+        )
+        .then(
+            (data) => {
+                movieData = data;
+                console.log(movieData);
+            }
+        )
+    }
 </script>
+
 
 <style>
     /* your styles go here */
 </style>
 
-<!-- markup (zero or more items) goes here -->
+
 
 <section class="filmHighlight">
     <h2 class="filmHightlight__title">Highlight</h2>
+    <button on:click={getFilmHighlight}>Request API</button>
 </section>
